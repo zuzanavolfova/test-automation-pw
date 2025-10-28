1f7fe9f1597102dde638aa38c9c61da078a02e8a
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-10-28 15:12:39 +0100

    Examples-lesson2

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -1,6 +1,13 @@
 import { test } from "@playwright/test";
 
 test("should open login page", async ({ page }) => {
-    await page.goto("/prihlaseni");
-    console.log(await page.title());
+  await page.goto("/prihlaseni");
+  await page.locator("input#email").screenshot({ path: "login-email.png" });
+  await page.locator(".btn-primary").screenshot({ path: "btn-primary.png" });
+  await page
+    .locator("div")
+    .locator("form")
+    .locator("input[type$='word']")
+    .screenshot({ path: "combine.png" });
+  console.log(await page.title());
 });

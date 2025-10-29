9a80ec38301634e25cbfdb43396d075b6fc5b6ad
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-10-29 19:08:12 +0100

    Add logout test

---
+++ src/tests/homework-lesson-5.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test("logIn-logOut", async ({ page }) => {
+  await page.goto("/prihlaseni");
+  const emailField = page.getByLabel("email");
+  const passwordField = page.getByLabel("Heslo");
+  const loginButton = page.getByRole("button", { name: "Přihlásit" });
+
+  await emailField.fill("zuzka.helesicova@email.cz");
+  await passwordField.fill("Czechitas123");
+  await loginButton.click();
+
+  const userName = page.locator(".navbar-right").locator("strong");
+  await userName.click();
+
+  const logoutButton = page.getByRole("link", { name: "Odhlásit" });
+  await logoutButton.waitFor();
+  await logoutButton.click();
+});
